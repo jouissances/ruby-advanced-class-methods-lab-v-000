b4c5ec80cfcfdbcd3b4b21b83ba09d41e584b45f
Janice Darikho <janicedarikho@gmail.com>
2018-05-31 11:40:16 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,5 @@
 class Song
-  attr_accessor :name, :artist_name
+  attr_accessor :name, :artist_name, :song
   @@all = []
 
   def self.all
@@ -9,5 +9,12 @@ class Song
   def save
     self.class.all << self
   end
-
+  
+  def self.create
+    self.new.save
+  end
+  
+  def self.new_by_name(name)
+    @name = name
+  end
 end
